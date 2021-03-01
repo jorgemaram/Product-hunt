b6a5e1704ed08720c2977b3706646f717f771926
Jorge Martin <jorgemaram@gmail.com>
2021-03-01 19:01:33 +0100

    styled components in header

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,27 +1,64 @@
 import React from 'react';
+import Link from 'next/link';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 import Buscar from '../ui/Buscar';
 import Navbar from './Navbar';
-import Link from 'next/link';
+import Boton from '../ui/Boton';
+
+const ContenedorHeader = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family:'Roboto Slab', serif;
+    margin-right: 2rem;
+
+`;
 
 
 const Header = () => {
-    return ( 
-        <header>
-            <div>
+
+    const user = false;
+
+    return (
+        <header css={css`
+            border-bottom:2px solid var(--gris3);
+            padding: 1rem 0;
+        `}>
+            <ContenedorHeader>
                 <div>
-                    <p>P</p>
-                    <Buscar/>
-                    <Navbar/>
+                    <Link href='/'>
+                        <Logo>P</Logo>
+                    </Link>
+                    <Buscar />
+                    <Navbar />
                 </div>
-                <div>
-                    <p>Hola Jorge</p>
-                    <button type='button'>Cerrar sesión</button>
-                    <Link href='/'>Login</Link>
-                    <Link href='/'>Crear cuenta</Link>
+                <div css={css`display:flex; align-items: center;`}>
+                    {user ? (
+                        <>
+                            <p css={css`margin-right: 2rem;`}>Hola Jorge</p>
+                            <Boton bgColor='true' n>Cerrar sesión</Boton>
+                        </>
+                    ) : (
+                        <>
+                            <Link href='/'><Boton bgColor='true'>Login</Boton></Link>
+                            <Link href='/'><Boton>Crear cuenta</Boton></Link>
+                        </>
+                        )}
                 </div>
-            </div>
+            </ContenedorHeader>
         </header>
-     );
+    );
 }
- 
+
 export default Header;
