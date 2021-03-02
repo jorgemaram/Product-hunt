e09c5d3ffab63c1942f23b3802f680c3efba40e2
Jorge Martin <jorgemaram@gmail.com>
2021-03-02 18:20:55 +0100

    adding Firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBSCZYyiAA-x1rMeEneyEbfo-pG6Lrh0yg",
+    authDomain: "product-hunt-2e98c.firebaseapp.com",
+    projectId: "product-hunt-2e98c",
+    storageBucket: "product-hunt-2e98c.appspot.com",
+    messagingSenderId: "76286578729",
+    appId: "1:76286578729:web:56945e382f2aacc1180c38",
+    measurementId: "G-3V8KGM18FZ"
+};
+
+export default firebaseConfig;
