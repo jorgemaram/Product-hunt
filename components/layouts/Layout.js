ecc77b62d346a52d6f26146c1c75ffde5789fc26
Jorge Martin <jorgemaram@gmail.com>
2021-03-04 12:25:56 +0100

    display products on homepage

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -49,7 +49,7 @@ const Layout = props => {
             <Head>
                 <html lang="es"/>
                 <title>Product Hunt Firebase y Next.js</title> 
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" />
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet" />
                 <link href='/static/css/app.css' rel='stylesheet'/>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,42 @@
-
-import React from 'react';
-import Layout from '../components/layouts/Layout'
-import styled from '@emotion/styled';
-
-const Heading = styled.h1`
-color:red;
-`;
+import React, {useEffect, useState, useContext} from 'react';
+import Layout from '../components/layouts/Layout';
+import DetallesProducto from '../components/layouts/DetallesProducto';
+import { FirebaseContext } from '../firebase';
 
 const Home = () => {
+
+  const [productos, guardarProductos] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarSnapshot);
+    }
+    obtenerProductos();
+  }, [])
+
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+    guardarProductos(productos);
+  }
+
   return (
     <div>
       <Layout>
-        <Heading>Inicio</Heading>
+        <div className='listado-productos'>
+          <div className='contenedor'>
+            <ul className='bg-white'>
+              {productos.map(producto => (
+                <DetallesProducto key={producto.id} producto={producto}/>
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   )
