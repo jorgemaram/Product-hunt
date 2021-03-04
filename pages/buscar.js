6b18f3e26c5dc8ba39b8b91b07f5fcbcdcd82a3d
Jorge Martin <jorgemaram@gmail.com>
2021-03-04 17:52:01 +0100

    search bar and display results

--- pages/buscar.js
+++ pages/buscar.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Layout from '../components/layouts/Layout'
-import styled from '@emotion/styled';
-
-const Heading = styled.h1`
-color:red;
-`;
+import { useRouter } from 'next/router';
+import DetallesProducto from '../components/layouts/DetallesProducto';
+import useProductos from '../hooks/useProductos';
 
 const Buscar = () => {
+
+    const router = useRouter();
+    const { query: { q } } = router;
+    
+    //Todos los productos 
+    const { productos } = useProductos('creado'); 
+    const [resultado, guardarResultado] = useState([]);
+
+    useEffect(() => {
+        const busqueda = q.toLowerCase();
+        const filtro = productos.filter(producto => {
+            return (
+                producto.nombre.toLowerCase().includes(busqueda) ||
+                producto.descripcion.toLowerCase().includes(busqueda)
+            )
+        })
+        guardarResultado(filtro)
+
+    }, [q, productos]);
+
     return (
         <div>
             <Layout>
-                <Heading>Buscar</Heading>
+                <div className='listado-productos'>
+                    <div className='contenedor'>
+                        <ul className='bg-white'>
+                            {resultado.map(producto => (
+                                <DetallesProducto key={producto.id} producto={producto} />
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </Layout>
         </div>
     )
